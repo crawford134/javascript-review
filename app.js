96db922bdaf4a664057b24bb7c2795a74353bd7e
crawford134 <38968995+crawford134@users.noreply.github.com>
2020-09-06 19:59:44 -0600

    Update app.js
    First attempt at the js. Adding a div to the html with js.

--- app.js
+++ app.js
@@ -6,19 +6,42 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 //Event Listeners 
-todoButton.addEventListen('click', addTodo); 
+todoButton.addEventListener('click', addTodo); 
 
 
 //Functions 
 
+
+/*
+This is how you add a new TO DO (Div) to the list/page/container. 
+*/
 function addTodo(event){
-  console.log("hello");
-
-
   //Prevent form from submitting 
   event.preventDefault();
 
-  // 
+  //Create the ToDiv 
+  const todoDiv = document.createElement("div"); 
+  todoDiv.classList.add("todo"); 
 
+  //Create the LI 
+  const newTodo = document.createElement("li");
+  newTodo.innerText = 'hey'; 
+  newTodo.classList.add("todo-item");   //this is used for css-styling 
+  todoDiv.appendChild(newTodo); 
+
+  //Create Completed Button 
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"> </i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  //Create Delete 
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"> </i>';
+  trashButton.classList.add("complete-btn"); 
+  todoDiv.appendChild(trashButton);
+
+  //APPEND TO LIST 
+  todoList.appendChild(todoDiv); 
 
 }
