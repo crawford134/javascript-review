e46021724db3496722ac16980b26254c388ddd74
crawford134 <38968995+crawford134@users.noreply.github.com>
2020-09-06 19:43:46 -0600

    Update app.js
    Added the beginning of the Javascript

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 
+
+//Selectors 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners 
+todoButton.addEventListen('click', addTodo); 
+
+
+//Functions 
+
+function addTodo(event){
+  console.log("hello");
+
+
+  //Prevent form from submitting 
+  event.preventDefault();
+
+  // 
+
+
+}
